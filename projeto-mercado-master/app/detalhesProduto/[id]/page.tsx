c3fa09fc013d3a4e3e6695fa6c8b9d1a18a50674
LariPato <166575018+LariPato@users.noreply.github.com>
2025-03-07 20:39:51 -0300

    arrumando essa bosta

--- projeto-mercado-master/app/detalhesProduto/[id]/page.tsx
+++ projeto-mercado-master/app/detalhesProduto/[id]/page.tsx
@@ -1,11 +1,25 @@
+import React from "react";
+import { listaData } from "@/data/listaData";
 
+type detalhesProduto = {
+    params: {
+        id: string;
+    }
+}
 
-function detalhesProduto (){
+const detalhesProduto = (props: detalhesProduto) => {
+    const produtoID = Number(props.params.id);
+    const produto = listaData.find(function (item) {
+        return item.id === produtoID;
+    });
     return(
         <>
             <h2>DETALHES PRODUTO</h2>
+            <p><strong>ID: </strong>{produto?.id}</p>
+            <p><strong>Nome: </strong>{produto?.nome}</p>
+            <p><strong>Quantidade: </strong>{produto?.quantidade}</p>
         </>
 
     )
-}
+};
 export default detalhesProduto;
